55f985d32ca2c1f1a5603efbd1ac761fb0fd790d
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-08-25 14:56:05 -0700

    Create map.js

---
+++ map.js
@@ -0,0 +1,11 @@
+
+
+const map = function(array, callback) {
+    const results = [];
+    for (let item of array) {
+     results.push(callback(item));
+    }
+    return results;
+  }
+//test case
+  const words = ["ground", "control", "to", "major", "tom"];
