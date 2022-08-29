50dddb9d926f201d95c3b125853f8cad83307398
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-08-29 10:52:08 -0700

    Delete lotide directory

--- lotide/reverse.js
+++
@@ -1,9 +0,0 @@
-let input = process.argv[2];
-
-const reverse = function(original) {
-  return original.split('').reverse().join('');
-};
-if (input)
-console.log(reverse(input));
-
-
--- lotide/test/headTest.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = require("../assertEqual");
-
-const head = function (parameter) {
-  let firstValue = parameter[0];
-  return firstValue;
-};
-
-module.exports = head;
