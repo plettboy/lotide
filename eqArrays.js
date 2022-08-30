ffc64b618435a8da61241d8b786ed914b1e7df19
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-08-29 20:44:33 -0700

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,7 @@
-const eqArrays = function(arr1, arr2) {
+const assertEqual = require("./assertEqual");
+
+const eqArrays = function (arr1, arr2) {
   return arr1.toString() === arr2.toString();
 };
-
 
 console.log(eqArrays([1], [2]));
